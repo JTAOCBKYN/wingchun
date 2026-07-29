a6e7f39e56f63f0289e24831da6a2b8a53b381d6
JTAOCBKYN <jacktobyn@icloud.com>
2026-07-29 22:01:30 +0100

    Update Hero.js

--- src/components/page/gallery/Hero.js
+++ src/components/page/gallery/Hero.js
@@ -5,7 +5,7 @@ const Hero = () => {
         <div className="container mx-auto h-full flex justify-start items-center">
           <div className="wrapper flex flex-col gap-[40px]">
             <h1 className="text-[50px] lg:text-[90px] leading-none text-white font-bold">Gallery</h1>
-            <p className="text-white/80 text-[20px] font-light text-left lg:max-w-[60%]">Discover the practice of Traditional Wing Chun through our video gallery, featuring class training, techniques, and demonstrations from our experienced instructors and students.</p>
+            <p className="text-white/80 text-[20px] font-light text-left lg:max-w-[60%]">Discover the practice of Traditional Wing Chun through our video gallery, featuring class training, techniques, and demonstrations from our experienced masters, instructors and students.</p>
           </div>
         </div>
         <div className="overlay absolute top-1/2 translate-y-[-50%] right-[-20px]">
