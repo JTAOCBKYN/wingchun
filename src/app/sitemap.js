8e6f6d9512342ebd09a2c1436117719c843c8d2e
JTAOCBKYN <jacktobyn@icloud.com>
2026-08-01 12:22:15 +0100

    Create sitemap.js

---
+++ src/app/sitemap.js
@@ -0,0 +1,21 @@
+export default function sitemap() {
+  const baseUrl = "https://www.smartwingchunuk.co.uk";
+
+  const routes = [
+    "",
+    "/classes",
+    "/syllabus",
+    "/history",
+    "/gallery",
+    "/articles",
+    "/locations",
+    "/contact",
+  ];
+
+  return routes.map((route) => ({
+    url: `${baseUrl}${route}`,
+    lastModified: new Date(),
+    changeFrequency: "monthly",
+    priority: route === "" ? 1 : 0.8,
+  }));
+}
